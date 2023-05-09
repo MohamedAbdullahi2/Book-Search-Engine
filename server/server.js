c4b983618e7bd640381507da98b4f9db0c27a8e0
MohamedAbdullahi2 <118404373+MohamedAbdullahi2@users.noreply.github.com>
2023-05-08 23:25:44 -0400

    final push

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const { ApolloServer } = require("apollo-server-express");
 
 // Importing the authentication middleware function from utils/auth
 const { authMiddleware } = require("./utils/auth");
+require("dotenv").config();
 
 const path = require("path");
 const db = require("./config/connection");
